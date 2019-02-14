4ae312f37c289ba040b6c16bdbecd3ae9b6429fa
Willian Praciano <willian.s.praciano@gmail.com>
2019-02-14 15:02:25 -0300

    Implementado atualização automática dos dados do usuário na barra lateral

--- app/src/main/java/praciano/willian/minhaufc/MainActivity.java
+++ app/src/main/java/praciano/willian/minhaufc/MainActivity.java
@@ -26,9 +26,7 @@ import praciano.willian.minhaufc.R;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private TextView textNome, textCurso, textMatricula;
-
-    private static String nome, matricula, curso;
+    private static TextView textNome, textCurso, textMatricula;
 
     private static String sigaaUrl = "https://si3.ufc.br/sigaa/verTelaLogin.do";
     private static String bibliotecaUrl = "https://pergamum.ufc.br/pergamum/mobile/index.php";
@@ -66,12 +64,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //Se o usuário estiver abrindo o app pela primeira vez então a StarActivity será iniciada
-        SharedPreferences preferences = getSharedPreferences(ARQUIVO_PREFERENCIAS, 0);
-        if (!preferences.contains("campus") || preferences.getString("campus", "").equals("")){
-            startActivity( new Intent(getApplicationContext(), PerfilActivity.class));
-        }
-
 
         //Configurações do FloatingActionButton
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -101,40 +93,176 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
 
-        //Definindo nome, curso e matricula no nav_header_main
+        //Obtendo id do nome, curso e matricula no nav_header_main
         View headerView = navigationView.getHeaderView(0);
         textNome = headerView.findViewById(R.id.textNome);
         textCurso = headerView.findViewById(R.id.textCurso);
         textMatricula = headerView.findViewById(R.id.textMatricula);
 
-        nome = preferences.getString("nome", "");
-        curso = preferences.getString("curso", "");
-        matricula = preferences.getString("matricula", "");
-
-        if(nome.equals("")){
-            textNome.setText("Minha UFC");
-        }else{
-            textNome.setText(nome);
-        }
-
-        if(curso.equals("")){
-            textCurso.setText("contato@loadingjr.com.br");
-        }else{
-            textCurso.setText(curso);
-        }
-
-        if(matricula.equals("")){
-            textMatricula.setText("Loading Jr.");
-        }else{
-            textMatricula.setText(matricula);
-        }
-
-
 
     }//fim do onCreate()
 
 
+    /*
+    Função que checa se o usuário já definiu o seu campus e atualiza os dados do usuário
+    na barra lateral
+     */
+    public void definindoUsuario(TextView tNome, TextView tCurso, TextView tMatricula){
 
+        //Se o usuário estiver abrindo o app pela primeira vez então a StarActivity será iniciada
+        SharedPreferences preferences = getSharedPreferences(ARQUIVO_PREFERENCIAS, 0);
+        if (!preferences.contains("campus") || preferences.getString("campus", "").equals("")){
+            startActivity( new Intent(getApplicationContext(), PerfilActivity.class));
+        }
+
+        String nome = preferences.getString("nome", "");
+        String curso = preferences.getString("curso", "");
+        String matricula = preferences.getString("matricula", "");
+
+        if(nome.equals("")){
+            tNome.setText("Minha UFC");
+        }else{
+            tNome.setText(nome);
+        }
+
+        if(curso.equals("")){
+            tCurso.setText("contato@loadingjr.com.br");
+        }else{
+            tCurso.setText(curso);
+        }
+
+        if(matricula.equals("")){
+            tMatricula.setText("Loading Jr.");
+        }else{
+            tMatricula.setText(matricula);
+        }
+    }
+
+
+    //Função que testa se o Chrome está instalado
+    private boolean chromeInstalled() {
+        try {
+
+            //getPackageManager().getPackageInfo("com.android.chrome", 0);
+            //return true;
+
+            return getPackageManager().getApplicationInfo("com.android.chrome", 0).enabled;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+
+    //Função para abrir links usando ChromeCustomTabs
+    public void abriChromeCustomTabs(String url){
+        if(chromeInstalled()){
+            CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+            CustomTabsIntent customTabsIntent = builder.build();
+            builder.setToolbarColor(Color.parseColor("#1098e7"));
+            builder.setShowTitle(true);
+            customTabsIntent.launchUrl(this, Uri.parse(url));
+        }else{
+            Toast.makeText(this, "O APP Funciona melhor com o Google Chrome intalado!!!", Toast.LENGTH_LONG).show();
+            abrirWebView(url, "SIGAA UFC");
+        }
+
+    }
+
+
+    //Função para abrir links usando WebView
+    public void abrirWebView(String url, String titulo){
+        Intent i = new Intent(this, WebViewActivity.class);
+        i.putExtra("keyUrl", url);
+        i.putExtra("keyTitulo", titulo);
+        startActivity(i);
+    }
+
+    //Função para abrir o SIGAA através de um botão
+    public void abrirSigaa(View view){
+        String url = sigaaUrl;
+        abriChromeCustomTabs(url);
+    }
+
+    //Função para abrir o site da Biblioteca da UFC através de um botão
+    public void abrirBiblioteca(View view){
+        String url = bibliotecaUrl;
+        String titulo = "Biblioteca";
+        abrirWebView(url, titulo);
+    }
+
+    //Função para abrir o site do cardapio do RU da UFC Campus Sobral através de um botão
+    public void abrirCardapio(View view){
+        SharedPreferences preferences = getSharedPreferences(ARQUIVO_PREFERENCIAS, 0);
+        int posicao = preferences.getInt("posicao", 1) - 1;
+        String url = cardapioUrl[posicao];
+        String titulo = "Cardápio RU";
+        abrirWebView(url, titulo);
+    }
+
+    //Função para abrir o site que mostra o saldo do Cartão do RU para alunos da UFC através de um botão
+    public void abrirCreditosRU(View view){
+        String url = creditosUrl;
+        String titulo = "Créditos RU";
+        abrirWebView(url, titulo);
+    }
+
+    //Função para abir um site com provas passadas através de um botão
+    public void abrirProvas(View view){
+
+        String url = provasUrl;
+        /*
+        abriChromeCustomTabs(url);
+        */
+        //startActivity(new Intent(MainActivity.this, WebViewActivity.class));
+        String titulo = "Provas Anteriores";
+        abrirWebView(url, titulo);
+    }
+
+    //Função para abrir o site de noticias da UFC Sobral através de um botão
+    public void abrirNoticias(View view){
+        SharedPreferences preferences = getSharedPreferences(ARQUIVO_PREFERENCIAS, 0);
+        int posicao = preferences.getInt("posicao", 1) - 1;
+        String url = noticiasUrl[posicao];
+        String titulo = "Notícias";
+        abrirWebView(url, titulo);
+    }
+
+    public void enviarProvas(){
+        String[] destinatario = {"willian.s.praciano@outlook.com"};
+
+        Intent i = new Intent(getIntent().ACTION_SENDTO);
+        i.setData(Uri.parse("mailto:"));
+        //i.setType("text/plain");
+        i.putExtra(getIntent().EXTRA_EMAIL, destinatario);
+        i.putExtra(getIntent().EXTRA_SUBJECT, "PROVAS ANTERIORES - SIGAM");
+        i.putExtra(getIntent().EXTRA_TEXT,
+                "Por favor, preencha os campos abaixo e anexe o(s) arquivo(s) da(s) prova(s) que você deseja enviar." + "\n\n" +
+                        "Curso: " + "\n" +
+                        "Disciplina: " + "\n" +
+                        "Professor: "  + "\n" +
+                        "Semestre (Ex.: 2018.2): "
+        );
+
+        //i.setType("message/rfc822");
+
+        try {
+            startActivity(Intent.createChooser(i, "Enviar as Provas pelo:"));
+            Log.i("Finished sending email.", "");
+        }
+        catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(MainActivity.this, "Não há clientes de email instalados!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    @Override
+    protected void onStart() {
+
+        //Chama a função definindoUsuario() todas as vez que a MainActivity for iniciada
+        definindoUsuario(textNome, textCurso, textMatricula);
+
+        super.onStart();
+    }
 
     //Definindo ações ao pressionar o botão de voltar do celular
     @Override
@@ -146,6 +274,7 @@ public class MainActivity extends AppCompatActivity
             super.onBackPressed();
         }
     }
+
 /*
     //Menus
     @Override
@@ -228,7 +357,7 @@ public class MainActivity extends AppCompatActivity
                     .setAction("Confirmar?", new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {*/
-                            enviarProvas();
+            enviarProvas();
             /*            }
                     })
                     .setActionTextColor(getResources().getColor(R.color.colorAccent))
@@ -255,124 +384,7 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
-    }
 
-
-    //Função que testa se o Chrome está instalado
-    private boolean chromeInstalled() {
-        try {
-
-            //getPackageManager().getPackageInfo("com.android.chrome", 0);
-            //return true;
-
-            return getPackageManager().getApplicationInfo("com.android.chrome", 0).enabled;
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-
-    //Função para abrir links usando ChromeCustomTabs
-    public void abriChromeCustomTabs(String url){
-        if(chromeInstalled()){
-            CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
-            CustomTabsIntent customTabsIntent = builder.build();
-            builder.setToolbarColor(Color.parseColor("#1098e7"));
-            builder.setShowTitle(true);
-            customTabsIntent.launchUrl(this, Uri.parse(url));
-        }else{
-            Toast.makeText(this, "O APP Funciona melhor com o Google Chrome intalado!!!", Toast.LENGTH_LONG).show();
-            abrirWebView(url, "SIGAA UFC");
-        }
-
-    }
-
-
-    //Função para abrir links usando WebView
-    public void abrirWebView(String url, String titulo){
-        Intent i = new Intent(this, WebViewActivity.class);
-        i.putExtra("keyUrl", url);
-        i.putExtra("keyTitulo", titulo);
-        startActivity(i);
-    }
-
-
-    //Função para abrir o SIGAA através de um botão
-    public void abrirSigaa(View view){
-        String url = sigaaUrl;
-        abriChromeCustomTabs(url);
-    }
-
-    //Função para abrir o site da Biblioteca da UFC através de um botão
-    public void abrirBiblioteca(View view){
-        String url = bibliotecaUrl;
-        String titulo = "Biblioteca";
-        abrirWebView(url, titulo);
-    }
-
-    //Função para abrir o site do cardapio do RU da UFC Campus Sobral através de um botão
-    public void abrirCardapio(View view){
-        SharedPreferences preferences = getSharedPreferences(ARQUIVO_PREFERENCIAS, 0);
-        int posicao = preferences.getInt("posicao", 1) - 1;
-        String url = cardapioUrl[posicao];
-        String titulo = "Cardápio RU";
-        abrirWebView(url, titulo);
-    }
-
-    //Função para abrir o site que mostra o saldo do Cartão do RU para alunos da UFC através de um botão
-    public void abrirCreditosRU(View view){
-        String url = creditosUrl;
-        String titulo = "Créditos RU";
-        abrirWebView(url, titulo);
-    }
-
-    //Função para abir um site com provas passadas através de um botão
-    public void abrirProvas(View view){
-
-        String url = provasUrl;
-        /*
-        abriChromeCustomTabs(url);
-        */
-        //startActivity(new Intent(MainActivity.this, WebViewActivity.class));
-        String titulo = "Provas Anteriores";
-        abrirWebView(url, titulo);
-    }
-
-    //Função para abrir o site de noticias da UFC Sobral através de um botão
-    public void abrirNoticias(View view){
-        SharedPreferences preferences = getSharedPreferences(ARQUIVO_PREFERENCIAS, 0);
-        int posicao = preferences.getInt("posicao", 1) - 1;
-        String url = noticiasUrl[posicao];
-        String titulo = "Notícias";
-        abrirWebView(url, titulo);
-    }
-
-    public void enviarProvas(){
-        String[] destinatario = {"willian.s.praciano@outlook.com"};
-
-        Intent i = new Intent(getIntent().ACTION_SENDTO);
-        i.setData(Uri.parse("mailto:"));
-        //i.setType("text/plain");
-        i.putExtra(getIntent().EXTRA_EMAIL, destinatario);
-        i.putExtra(getIntent().EXTRA_SUBJECT, "PROVAS ANTERIORES - SIGAM");
-        i.putExtra(getIntent().EXTRA_TEXT,
-                "Por favor, preencha os campos abaixo e anexe o(s) arquivo(s) da(s) prova(s) que você deseja enviar." + "\n\n" +
-                        "Curso: " + "\n" +
-                        "Disciplina: " + "\n" +
-                        "Professor: "  + "\n" +
-                        "Semestre (Ex.: 2018.2): "
-        );
-
-        //i.setType("message/rfc822");
-
-        try {
-            startActivity(Intent.createChooser(i, "Enviar as Provas pelo:"));
-            Log.i("Finished sending email.", "");
-        }
-        catch (android.content.ActivityNotFoundException ex) {
-            Toast.makeText(MainActivity.this, "Não há clientes de email instalados!", Toast.LENGTH_SHORT).show();
-        }
-    }
-
+    }//Fim do onNavigationItemSelected()
 
 }
